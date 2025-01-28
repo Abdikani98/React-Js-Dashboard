fe8c89a5d3814a2b4e12e33d18f4b4db58a8169f
Abdikani98 <abdikanishaarib>
2025-01-27 20:31:33 -0800

    update

--- src/Pages/Teacher.jsx
+++ src/Pages/Teacher.jsx
@@ -1,29 +1,6 @@
 import React from "react";
 
 function Teacher() {
-  const teachers = [
-    {
-      id: 1,
-      name: "Abdi Ali",
-      email: "Abdi@example.com",
-      subject: "Mathematics",
-      phone: "252618947834",
-    },
-    {
-      id: 2,
-      name: "Abdi Ali",
-      email: "Abdi@example.com",
-      subject: "Physics",
-      phone: "252618947834",
-    },
-    {
-      id: 3,
-      name: "Sadaq adan xasan",
-      email: "Sadaq@example.com",
-      subject: "PhysiEnglishs",
-      phone: "252616342997",
-    },
-  ];
   return (
     <div className="flex gap-8  justify-center  bg-gray-100 min-h-screen fixed left-[23%] w-[80%] items-center">
       {/* Card 1 */}
